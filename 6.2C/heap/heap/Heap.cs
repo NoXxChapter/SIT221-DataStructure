c13136460f4abebfe82c55e5406160b01db4202a
NoXxChapter <ngotungchuong15@gmail.com>
2026-04-26 20:30:58 +1000

    Add files via upload

---
+++ 6.2C/heap/heap/Heap.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heap
+{
+    public class Heap<K, D> where K : IComparable<K>
+    {
+
+        // This is a nested Node class whose purpose is to represent a node of a heap.
+        private class Node : IHeapifyable<K, D>
+        {
+            // The Data field represents a payload.
+            public D Data { get; set; }
+            // The Key field is used to order elements with regard to the Binary Min (Max) Heap Policy, i.e. the key of the parent node is smaller (larger) than the key of its children.
+            public K Key { get; set; }
+            // The Position field reflects the location (index) of the node in the array-based internal data structure.
+            public int Position { get; set; }
+
+            public Node(K key, D value, int position)
+            {
+                Data = value;
+                Key = key;
+                Position = position;
+            }
+
+            // This is a ToString() method of the Node class.
+            // It prints out a node as a tuple ('key value','payload','index')}.
+            public override string ToString()
+            {
+                return "(" + Key.ToString() + "," + Data.ToString() + "," + Position + ")";
+            }
+        }
+
+        // ---------------------------------------------------------------------------------
+        // Here the description of the methods and attributes of the Heap<K, D> class starts
+
+        public int Count { get; private set; }
+
+        // The data nodes of the Heap<K, D> are stored internally in the List collection. 
+        // Note that the element with index 0 is a dummy node.
+        // The top-most element of the heap returned to the user via Min() is indexed as 1.
+        private List<Node> data = new List<Node>();
+
+        // We refer to a given comparer to order elements in the heap. 
+        // Depending on the comparer, we may get either a binary Min-Heap or a binary  Max-Heap. 
+        // In the former case, the comparer must order elements in the ascending order of the keys, and does this in the descending order in the latter case.
+        private IComparer<K> comparer;
+
+        // We expect the user to specify the comparer via the given argument.
+        public Heap(IComparer<K> comparer)
+        {
+            this.comparer = comparer;
+
+            // We use a default comparer when the user is unable to provide one. 
+            // This implies the restriction on type K such as 'where K : IComparable<K>' in the class declaration.
+            if (this.comparer == null) this.comparer = Comparer<K>.Default;
+
+            // We simplify the implementation of the Heap<K, D> by creating a dummy node at position 0.
+            // This allows to achieve the following property:
+            // The children of a node with index i have indices 2*i and 2*i+1 (if they exist).
+            data.Add(new Node(default(K), default(D), 0));
+        }
+
+        // This method returns the top-most (either a minimum or a maximum) of the heap.
+        // It does not delete the element, just returns the node casted to the IHeapifyable<K, D> interface.
+        public IHeapifyable<K, D> Min()
+        {
+            if (Count == 0) throw new InvalidOperationException("The heap is empty.");
+            return data[1];
+        }
+
+        // Insertion to the Heap<K, D> is based on the private UpHeap() method
+        public IHeapifyable<K, D> Insert(K key, D value)
+        {
+            Count++;
+            Node node = new Node(key, value, Count);
+            data.Add(node);
+            UpHeap(Count);
+            return node;
+        }
+
+        private void UpHeap(int start)
+        {
+            int position = start;
+            while (position != 1)
+            {
+                if (comparer.Compare(data[position].Key, data[position / 2].Key) < 0) Swap(position, position / 2);
+                position = position / 2;
+            }
+        }
+
+        // This method swaps two elements in the list representing the heap. 
+        // Use it when you need to swap nodes in your solution, e.g. in DownHeap() that you will need to develop.
+        private void Swap(int from, int to)
+        {
+            Node temp = data[from];
+            data[from] = data[to];
+            data[to] = temp;
+            data[to].Position = to;
+            data[from].Position = from;
+        }
+
+        public void Clear()
+        {
+            for (int i = 0; i<=Count; i++) data[i].Position = -1;
+            data.Clear();
+            data.Add(new Node(default(K), default(D), 0));
+            Count = 0;
+        }
+
+        public override string ToString()
+        {
+            if (Count == 0) return "[]";
+            StringBuilder s = new StringBuilder();
+            s.Append("[");
+            for (int i = 0; i < Count; i++)
+            {
+                s.Append(data[i + 1]);
+                if (i + 1 < Count) s.Append(",");
+            }
+            s.Append("]");
+            return s.ToString();
+        }
+
+        // TODO: Your task is to implement all the remaining methods.
+        // Read the instruction carefully, study the code examples from above as they should help you to write the rest of the code.
+
+        // left child is 2*i, right child is 2*i+1, parent is i/2
+        public IHeapifyable<K, D> Delete()
+        {
+            if (Count == 0) 
+                throw new InvalidOperationException();
+            IHeapifyable<K, D> min = data[1];
+            Swap(1, Count); // swap the top-most element with the last one in the heap
+            data.RemoveAt(Count);   // remove the last element (the minimum) from the heap
+            Count--;    
+            if (Count > 0)  
+                DownHeap(1);    // push the element that was moved to the root position down to its correct level
+            return min;
+        }
+
+        private void DownHeap(int v)
+        {
+            while (2 * v <= Count)  // while the node with index v has at least one child
+            {
+                int j = 2 * v;  // j is the index of the left child of v
+                if (j < Count && comparer.Compare(data[j].Key, data[j + 1].Key) > 0)    // if the right child of v exists 
+                                                                                        // and smaller than the left child
+                    j++; // update j to the index of the smallest child of v
+                if (comparer.Compare(data[v].Key, data[j].Key) <= 0)    // if node v is smaller than its smallest child, 
+                                                                        // then the heap property is satisfied 
+                    break;
+
+                Swap(v, j);
+                v = j;
+            }
+        }
+
+        // Builds a minimum binary heap using the specified data according to the bottom-up approach.
+        public IHeapifyable<K, D>[] BuildHeap(K[] keys, D[] data)
+        {
+            if (Count > 0) 
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (keys.Length != data.Length) 
+                throw new ArgumentException();
+
+            // create the result array to store nodes in their ORIGINAL input order
+            IHeapifyable<K, D>[] result = new IHeapifyable<K, D>[keys.Length];
+
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Count++;
+                Node newNode = new Node(keys[i], data[i], Count);
+                this.data.Add(newNode);
+                
+                // store the node in the result array at the same index 'i' as the input
+                result[i] = newNode;
+            }
+
+            // perform Floyd's bottom-up heap construction
+            for (int i = Count / 2; i >= 1; i--)
+            {
+                DownHeap(i);
+            }
+
+            // return the array containing nodes in the order they were provided
+            return result;
+        }
+
+        public void DecreaseKey(IHeapifyable<K, D> element, K new_key)
+        {
+            Node node = element as Node;
+            
+            if (node == null || node.Position <= 0 || node.Position > Count || data[node.Position] != node)
+            {
+                throw new InvalidOperationException();
+            }
+
+            // check the new key 
+            int comparison = comparer.Compare(new_key, node.Key);
+            node.Key = new_key;
+
+            if (comparison < 0)
+            {
+                // UpHeap for Min, DownHeap for Max
+                UpHeap(node.Position);
+                DownHeap(node.Position); 
+            }
+            else if (comparison > 0)
+            {
+                // DownHeap for Min, UpHeap for Max
+                DownHeap(node.Position);
+                UpHeap(node.Position);
+            }
+        }
+
+    }
+}
